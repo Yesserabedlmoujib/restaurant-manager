3fcd716d16fff1da524cb2ce13273224816214fa
Yesser <mhamdiyesser04@gmail.com>
2026-07-14 19:58:25 +0100

    feat: setup tailwind and ui utilities

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div>
-      <h1>Restaurant Manager</h1>
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-green-800">Restaurant Manager</h1>
     </div>
   );
 }
