d0a048b796f5c85ef246c878fff17c1d5b58c7dd
Yesser <mhamdiyesser04@gmail.com>
2026-07-14 20:29:51 +0100

    feat: setup tanstack router

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div>
+            <h1>Restaurant Admin</h1>
+            <p>Welcome to the admin console</p>
+        </div>
+    )
+}
+
+export default Home
