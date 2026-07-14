74191c7270c8635c94a885459b632628ae1029a8
Yesser <mhamdiyesser04@gmail.com>
2026-07-14 20:36:29 +0100

    feat: setup react query provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import { RouterProvider } from '@tanstack/react-router';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import { QueryProvider } from './providers/QueryProvider';
 import { router } from "./router";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryProvider>
+      <RouterProvider router={router} />
+    </QueryProvider>
   </StrictMode>,
 )
